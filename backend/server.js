48936b9a0cbf7ca2253d39fea5fadce6b7ae5196
pamela2002 <sroychoudhury65@gmail.com>
2024-04-19 02:52:28 +0530

    Sign Up Endpoint created

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-import authRoutes from "./routes/auth.routes.js"
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World");
+// })
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
