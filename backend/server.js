46be1d371371e622e88e8560b41b4c0edbbd9dd0
pamela2002 <sroychoudhury65@gmail.com>
2024-04-29 22:07:14 +0530

    Send Message Endpoint created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes."
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,7 +12,9 @@ dotenv.config();
 
 app.use(express.json());
 
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 // app.get('/', (req, res) => {
