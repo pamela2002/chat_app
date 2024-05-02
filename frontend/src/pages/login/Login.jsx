ccc6bd7a8ec95a29774bf3ccf7fc737e749a96f9
pamela2002 <sroychoudhury65@gmail.com>
2024-05-02 23:20:37 +0530

    UI design of SignUp page and Login page done

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -7,7 +7,37 @@ const Login = () => {
               <h1 className='text-3xl font-semibold text-center text-gray-300'>
                   Login
                   <span className='text-blue-500'> ChatApp</span>
-              </h1>
+        </h1>
+        <form>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Enter username'
+              className='w-full input input-bordered h-10'
+            />
+          </div>
+          <div>
+            <label className='label'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input
+              type='password'
+              placeholder='Enter Password'
+              className='w-full input input-bordered h-10'
+            />
+          </div>
+          <a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+            {"Don't"} have an account?
+          </a>
+          <div>
+            <button className='btn btn-block btn-sm mt-2'>
+               Login
+            </button>
+          </div>
+        </form>
           </div>
     
     </div>
