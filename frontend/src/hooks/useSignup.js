01a33366bebaca6c9fdbdb3d6b0ea103403165d1
pamela2002 <sroychoudhury65@gmail.com>
2024-05-17 00:00:56 +0530

    SignUp Functionality partially completed

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+const useSignup = () => {
+    const [loading, setLoading] = useState(false);
+    
+    const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
+        
+     }
+}
+
+export default useSignup
