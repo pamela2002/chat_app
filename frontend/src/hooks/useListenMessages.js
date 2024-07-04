33e3a0710fbc9687d41d536bcaddb978f69ff96f
pamela2002 <sroychoudhury65@gmail.com>
2024-07-05 03:22:07 +0530

    deployment

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -14,9 +14,11 @@ function useListenMessages() {
             sound.play();
             setMessages([...messages, newMessage]);
         })
-
+        // Cleaning Up: The socket?.off("newMessage") method removes the event listener when the component unmounts or the dependencies change, preventing memory leaks and duplicate listeners.
         return () => socket?.off("newMessage")
     },[socket, setMessages, messages])
 }
 
 export default useListenMessages
+
+
